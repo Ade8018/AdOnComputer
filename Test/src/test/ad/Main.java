4735e1011d275b879f2ed6e8016904c8cd091e27
Ade8018 <tomatomaster@hotmail.com>
2016-03-15 16:43:58 +0800

    测试

--- Test/src/test/ad/Main.java
+++ Test/src/test/ad/Main.java
@@ -4,9 +4,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
@@ -24,71 +22,77 @@ public class Main {
 		String province = Utils.getProvince(resp);
 		r.adId = adids[0];
 		r.province = province;
-		
-		Request desktop = Request.get("desktop", "9");
-		System.out.println(desktop.toString());
-		resp = postInfo(
-				Base64.encodeToString(desktop.toString().getBytes(), 0), "a");
-		System.out.println(resp);
-		
-		Request chaping1 = Request.get("chaping1", "1");
-		System.out.println(chaping1.toString());
-		resp = postInfo(
-				Base64.encodeToString(chaping1.toString().getBytes(), 0), "a");
-		System.out.println(resp);
-		
-		Request newDesktop = Request.get("newDesktop", "8");
-		System.out.println(newDesktop.toString());
-		resp = postInfo(
-				Base64.encodeToString(newDesktop.toString().getBytes(), 0), "a");
-		System.out.println(resp);
-		
-		Request push = Request.get("push", "1");
-		System.out.println(push.toString());
-		resp = postInfo(
-				Base64.encodeToString(push.toString().getBytes(), 0), "a");
-		System.out.println(resp);
 //		
+//		Request desktop = Request.get("desktop", "9");
+//		System.out.println(desktop.toString());
+//		resp = postInfo(
+//				Base64.encodeToString(desktop.toString().getBytes(), 0), "a");
+//		System.out.println(resp);
 //		
-//		try {
-//			Thread.sleep(100);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-//		Action a = new Action(r, "push");
-//		System.out.println(a.toString());
-//		resp = postInfo(Base64.encodeToString(a.toString().getBytes(), 0), "b");
+//		Request chaping1 = Request.get("chaping1", "1");
+//		System.out.println(chaping1.toString());
+//		resp = postInfo(
+//				Base64.encodeToString(chaping1.toString().getBytes(), 0), "a");
 //		System.out.println(resp);
-//
-//		try {
-//			Thread.sleep(2000);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-//		a.action = "download";
-//		System.out.println(a.toString());
-//		resp = postInfo(Base64.encodeToString(a.toString().getBytes(), 0), "b");
+//		
+//		Request newDesktop = Request.get("newDesktop", "8");
+//		System.out.println(newDesktop.toString());
+//		resp = postInfo(
+//				Base64.encodeToString(newDesktop.toString().getBytes(), 0), "a");
 //		System.out.println(resp);
-//
-//		try {
-//			Thread.sleep(12000);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-//		a.action = "install";
-//		System.out.println(a.toString());
-//		resp = postInfo(Base64.encodeToString(a.toString().getBytes(), 0), "b");
+//		
+//		Request push = Request.get("push", "1");
+//		System.out.println(push.toString());
+//		resp = postInfo(
+//				Base64.encodeToString(push.toString().getBytes(), 0), "a");
 //		System.out.println(resp);
-//
-//		try {
-//			Thread.sleep(8000);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-//		a.action = "start";
-//		System.out.println(a.toString());
-//		resp = postInfo(Base64.encodeToString(a.toString().getBytes(), 0), "b");
-//		System.out.println(resp);
+		
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		Action a = new Action(r, "push");
+		System.out.println(a.toString());
+		resp = postInfo(Base64.encodeToString(a.toString().getBytes(), 0), "b");
+		System.out.println(resp);
+		
+		r.adId = adids[1];
+		System.out.println(r.toString());
+		resp = postInfo(Base64.encodeToString(r.toString().getBytes(), 0), "a");
+		System.out.println(resp);
+		
+		r.adId = adids[0];
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		a.action = "download";
+		System.out.println(a.toString());
+		resp = postInfo(Base64.encodeToString(a.toString().getBytes(), 0), "b");
+		System.out.println(resp);
+
+		try {
+			Thread.sleep(12000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		a.action = "install";
+		System.out.println(a.toString());
+		resp = postInfo(Base64.encodeToString(a.toString().getBytes(), 0), "b");
+		System.out.println(resp);
+
+		try {
+			Thread.sleep(8000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		a.action = "start";
+		System.out.println(a.toString());
+		resp = postInfo(Base64.encodeToString(a.toString().getBytes(), 0), "b");
+		System.out.println(resp);
 	}
 
 	private static String postInfo(String param, String method) {
